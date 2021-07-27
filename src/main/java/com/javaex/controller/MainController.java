51eefa4ffced00d6cef393e8c7099b00bd05d3b5
wonho <wonho951@gmail.com>
2021-07-27 10:28:06 +0900

    2021.07.27 수업내용(mysite5)

---
+++ src/main/java/com/javaex/controller/MainController.java
@@ -0,0 +1,31 @@
+package com.javaex.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class MainController {
+
+	//필드
+	//생성자
+	//메소드-gs
+	//메소드-일반
+	
+	//초반세팅 해두고 컨트롤러에서 테스트 한번 해보기
+	@RequestMapping(value = "/main", method = {RequestMethod.GET, RequestMethod.POST})
+	public String main() {
+		System.out.println("[MainController.main()]");
+		
+		return "";
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
