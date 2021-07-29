81699e1625f75beb853f578ea25cba3fae213e3a
wonho <wonho951@gmail.com>
2021-07-29 20:54:52 +0900

    2021.07.29 과제(mysite - guestbook)

--- src/main/java/com/javaex/dao/GuestbookDao.java
+++ src/main/java/com/javaex/dao/GuestbookDao.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.javaex.vo.GuestbookVo;
 
@@ -31,5 +32,13 @@ public class GuestbookDao {
 		System.out.println("dao 등록");
 		
 		return sqlSession.insert("guestbook.guestInsert", guestbookVo);
+	}
+	
+	
+	//삭제
+	public String delete(@RequestParam("no") int no, @RequestParam("password") String password) {
+		
+		
+		return
 	}
 }
