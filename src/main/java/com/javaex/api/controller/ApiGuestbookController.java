63975c4f9e75d8cd026a8a299d111e40b3e9d049
wonho <wonho951@gmail.com>
2021-08-03 12:03:37 +0900

    2021.08.03 수업내용(ajax)

--- src/main/java/com/javaex/api/controller/ApiGuestbookController.java
+++ src/main/java/com/javaex/api/controller/ApiGuestbookController.java
@@ -1,17 +1,33 @@
 package com.javaex.api.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.javaex.service.GuestbookService;
+import com.javaex.vo.GuestbookVo;
 
 @Controller
 public class ApiGuestbookController {
 
+	@Autowired
+	private GuestbookService guestService;
+	
+	
 	//ajax방식으로 리스트 가져오기
+	@ResponseBody	//리턴에 보낸 리턴값을 ResponseBody에 보내라
 	@RequestMapping(value = "/api/guestbook/list", method = {RequestMethod.GET, RequestMethod.POST})
-	public String List() {
+	public List<GuestbookVo> List() {
 		System.out.println("[ApiGuestbookController.list()]");
 		
-		return "";
+		List<GuestbookVo> guestbookList= guestService.list();
+		
+		System.out.println(guestbookList);
+		
+		return guestbookList;
 	}
 }
