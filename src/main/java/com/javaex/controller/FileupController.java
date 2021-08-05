258b9de93fe1b81139b77a86d2385c7e9bce188a
wonho <wonho951@gmail.com>
2021-08-05 12:38:55 +0900

    2021.08.05 수업내용(fileupload)

--- src/main/java/com/javaex/controller/FileupController.java
+++ src/main/java/com/javaex/controller/FileupController.java
@@ -20,7 +20,7 @@ public class FileupController {
    //파일폼 업로드 화면
    @RequestMapping(value = "/form", method = {RequestMethod.GET, RequestMethod.POST})
    public String form() {
-      System.out.println("FileupController.form()");
+      //System.out.println("FileupController.form()");
       
       return "/fileup/form";
    }
@@ -29,7 +29,7 @@ public class FileupController {
    //파일 업로드
    @RequestMapping(value = "/upload", method = {RequestMethod.GET, RequestMethod.POST})
    public String upload(@RequestParam("file") MultipartFile file) {
-      System.out.println("FileupController.upload()");
+      //System.out.println("FileupController.upload()");
       
       //System.out.println(file.getOriginalFilename());      //실제파일명
      // System.out.println(file.getSize());               //파일사이즈
--- src/main/java/com/javaex/service/FileupService.java
+++ src/main/java/com/javaex/service/FileupService.java
@@ -13,6 +13,9 @@ public class FileupService {
    public void restore(MultipartFile file) {
       System.out.println("FileupController.restore()");
       
+      //파일 경로가 바뀔수도 있으니 따로 빼둠
+      String saveDir = "C\\javaStudy\\upload";
+      
       //System.out.println(file.getOriginalFilename());
       //System.out.println(file.getSize());
       
@@ -21,23 +24,40 @@ public class FileupService {
       
       //원파일 이름
       String orgName = file.getOriginalFilename();
-      System.out.println(orgName);
+      System.out.println("orgName : " + orgName);
       
       //확장자
       String exName = file.getOriginalFilename().substring(   file.getOriginalFilename().lastIndexOf(".")    );	//kang.jpg (substring은 .기준(원파일 이름)으로 앞부분 날리고 뒷부분(확장자)구해라 
-      System.out.println(exName);	//.jpg(확장자 나옴)
+      System.out.println("exName : " + exName);	//.jpg(확장자 나옴)
       
       //저장파일 이름(관리때문에 겹치지 않는 새이름 부여)
       //이름 절대 안겹쳐야함.
       //이 조합으로 함 (시간 기준 + 랜덤 아이디 + 확장자)로 조합해서 안겹치게 만든다.(생각해낸 사람 대단하네)
       String saveName = System.currentTimeMillis() + UUID.randomUUID().toString() + exName;	//얘는 tostring 까지 써야함. -> 이름 안겹치게 랜덤으로 만들어줌(근데 재수나쁘면 같은애 나올 수도 있음. 이론적으로는 로또당첨보다 힘들긴함;)
       
-      System.out.println(saveName);
+      System.out.println("saveName : " + saveName);
       
       
-      //파일패스
+      //파일패스(경로) -> 파일을 저장할 위치 + 파일명.(파일 위치는 따로 빠질수도 있기 때문에 위에 따로 빼둠)
+      String filePath = saveDir + "\\" + saveName;
+      System.out.println("filePath : " + filePath);
       
       //파일 사이즈
+      long fileSize = file.getSize();
+      System.out.println("fileSize : " + fileSize);
+      
+      
+      
+      
+      
+      //파일을 서버 하드디스크에 저장
+      
+      
+      
+      
+      
+      
+      
       
    }
    
